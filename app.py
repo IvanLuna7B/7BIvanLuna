825d15ff7458085c9c0479f8077b3e39c97cee68
IvanLuna7B <ivanorlando875@gmail.com>
2024-09-23 20:21:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
-from flask import Flask
-
-from flask import render_template
-from flask import request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Ruta para la página principal
 @app.route("/")
 def index():
     return "<p>Hola, Mundo!</p>"
 
+# Ruta para la página de alumnos
 @app.route("/alumnos")
 def alumnos():
     return render_template("alumnos.html")
 
+# Ruta para guardar los datos de los alumnos (recibe datos por POST)
 @app.route("/alumnos/guardar", methods=["POST"])
-def alumnosGuardar():
-    return f"Matrícula {request.form["txtMatriculaFA"]} Nombre y Apellido {request.form["txtNombreApellidoFA"]}"
+def alumnosGuardar(): 
+    matricula = request.form['txtMatriculaFA']
+    nombre_apellido = request.form['txtNombreApellidoFA']
+    return f"Matrícula: {matricula}, Nombre y Apellido: {nombre_apellido}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
